26d4c895a8f123e0dfb807bc0172bcac53af0ca7
lanmai14 <lan.mai@zodinet.tech>
2021-11-19 16:53:38 +0700

    get and get api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,42 +1,15 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  getAll() {
+    return this.userService.getAll();
   }
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  getOneByID() {
+    return this.userService.getOneByID(1);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  usersRepository: any;
+  constructor(
+    @InjectRepository(User)
+    private readonly feedPostRepository: Repository<User>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.usersRepository.find(); //Select * from user
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async getOneByID(id: number): Promise<User> {
+    try {
+      const user = await this.usersRepository.findOneByID(id);
+      return user;
+    } catch (err) {
+      // hand error
+      throw err;
+    }
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // creaateUser(name: string): Promise<User> {
+  //   const newUser = this.usersRepository.create({ name }); //const newUser = new User(name);
+  //   return this.usersRepository.save(newUser);
+  // }
+  // updateUser(id:name, name:string):
 }
