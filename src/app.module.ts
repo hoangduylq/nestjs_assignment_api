3c32882435e2c3ab4a40fd946d3af81bdfeb7ad2
ngando2028 <ngan.do@zodinet.tech>
2021-11-19 15:22:37 +0700

    config connect DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRE_HOST,
+      port: parseInt(<string>process.env.POSTGRE_PORT),
+      username: process.env.POSTGRES_USER,
+      password: <string>process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+      // extra: { ssl: { rejectUnauthorized: true } },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
