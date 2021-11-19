aaae5e75c086ad13b8550c34001aaf2a9ed933c5
ngando2028 <ngan.do@zodinet.tech>
2021-11-19 15:53:48 +0700

    create entity user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+export class CreateUserDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @MinLength(6)
+  username: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  birthday: Date;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  birthday: Date;
+}
