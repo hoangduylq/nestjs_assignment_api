b6725480c2f9cb4a0726e563db4dc728ce59444d
lanmai14 <lan.mai@zodinet.tech>
2021-11-22 20:34:50 +0700

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,51 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiCreatedResponse({ type: CreateUserDto })
+  @ApiBadRequestResponse()
+  @Post('register')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @ApiCreatedResponse({ type: UpdateUserDto })
+  @ApiBadRequestResponse()
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
+
   @Get()
   getAll() {
-    return this.userService.getAll();
+    return this.userService.getAllUser();
   }
 
   @Get(':id')
